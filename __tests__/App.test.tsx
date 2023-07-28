ee83da90b4fc22f755d41b83d646ce6b8be096fc
twinedo <twinedo.dev@gmail.com>
2023-07-28 17:52:05 +0700

    config

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,11 +7,15 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+import {test, expect} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
+import {Detail} from 'pages';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', () => {
+  const {getByText} = render(<Detail />);
+  const textElement = getByText('Hello, World!');
+  expect(textElement).toBeDefined();
 });
